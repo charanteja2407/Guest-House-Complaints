52f47dfb709bae3b1e3d34a04841b281440ff368
charantejanaik <68796160+charanteja2407@users.noreply.github.com>
2022-05-02 02:09:14 +0530

    Uploaded files

---
+++ models/complaint-mapping.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const dbconnect = require('../db')
+
+//Call the db to connect the mongo db
+dbconnect()
+
+// Complaint Schema
+const ComplaintMappingSchema = mongoose.Schema({
+    complaintID: {
+        type: String,
+        required: true
+    },
+    engineerName: {
+        type: String,
+        required: true
+    },
+});
+
+const ComplaintMapping = module.exports = mongoose.model('ComplaintMapping', ComplaintMappingSchema);
+
+module.exports.registerMapping = function (newComplaintMapping, callback) {
+    newComplaintMapping.save(callback);
+}
+
+
+module.exports.getAllComplaintMappings = function(callback){
+    ComplaintMapping.find(callback).lean();
+  }
+
+module.exports.getCompleteMappingDetails = function(callback) {
+
+}
